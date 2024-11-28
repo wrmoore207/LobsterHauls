ab17a7b3c847f9a907760e7cb2f0fbfa13870387
Ryan Moore <144742064+wrmoore207@users.noreply.github.com>
2024-11-28 08:10:58 -0500

    Updates to Readme.md

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -20,55 +20,56 @@ pio.kaleido.scope.default_format = "svg"
 file_path = "Data\countyLobsters.txt"
 data = pd.read_csv(file_path, delim_whitespace=True)
 
-'''
-Preprocessing and Filtering
-'''
-# Convert the 'POUNDS' column to integers
-data['POUNDS'] = data['POUNDS'].str.replace(',', '').astype(int)
+print(data.shape)
+# '''
+# Preprocessing and Filtering
+# '''
+# # Convert the 'POUNDS' column to integers
+# data['POUNDS'] = data['POUNDS'].str.replace(',', '').astype(int)
 
-# Remove the dollar signs and commas, then convert to integers
-data['VALUE'] = data['VALUE'].str.replace('[\$,]', '', regex=True).astype(int)
+# # Remove the dollar signs and commas, then convert to integers
+# data['VALUE'] = data['VALUE'].str.replace('[\$,]', '', regex=True).astype(int)
 
-# List of counties to drop
-counties_to_drop = ['NOT-SPECIFIED', 'KNOX/WALDO', 'UNKNOWN', 'TOTAL']
+# # List of counties to drop
+# counties_to_drop = ['NOT-SPECIFIED', 'KNOX/WALDO', 'UNKNOWN', 'TOTAL']
 
-# Filter the data to exclude the specified counties
-filtered_data = data[~data['COUNTY'].isin(counties_to_drop)]
+# # Filter the data to exclude the specified counties
+# filtered_data = data[~data['COUNTY'].isin(counties_to_drop)]
 
-# Update the COUNTY column to have title case
-filtered_data['COUNTY'] = filtered_data['COUNTY'].str.title()
+# # Update the COUNTY column to have title case
+# filtered_data['COUNTY'] = filtered_data['COUNTY'].str.title()
 
-county_to_fips = {
-    "Androscoggin": "001",
-    "Aroostook": "003",
-    "Cumberland": "005",
-    "Franklin": "007",
-    "Hancock": "009",
-    "Kennebec": "011",
-    "Knox": "013",
-    "Lincoln": "015",
-    "Oxford": "017",
-    "Penobscot": "019",
-    "Piscataquis": "021",
-    "Sagadahoc": "023",
-    "Somerset": "025",
-    "Waldo": "027",
-    "Washington": "029",
-    "York": "031"
-}
+# county_to_fips = {
+#     "Androscoggin": "001",
+#     "Aroostook": "003",
+#     "Cumberland": "005",
+#     "Franklin": "007",
+#     "Hancock": "009",
+#     "Kennebec": "011",
+#     "Knox": "013",
+#     "Lincoln": "015",
+#     "Oxford": "017",
+#     "Penobscot": "019",
+#     "Piscataquis": "021",
+#     "Sagadahoc": "023",
+#     "Somerset": "025",
+#     "Waldo": "027",
+#     "Washington": "029",
+#     "York": "031"
+# }
 
-filtered_data['FIPS'] = filtered_data['COUNTY'].map(county_to_fips)
+# filtered_data['FIPS'] = filtered_data['COUNTY'].map(county_to_fips)
 
 
 
-'''
-Visualizations
-    [x] Annual Catch Over Time
-    [x] Annual Catch by County Over Time
-    [x] Value per Pound per county (Value/Pounds)
-    [] Annual Value Over Time
-    [] Choropleth of Value by County for most recent year
-'''
+# '''
+# Visualizations
+#     [x] Annual Catch Over Time
+#     [x] Annual Catch by County Over Time
+#     [x] Value per Pound per county (Value/Pounds)
+#     [] Annual Value Over Time
+#     [] Value 
+# '''
 
 # # [x] Annual Catch by County Over Time
 # plt.figure(figsize=(12, 8))
@@ -88,6 +89,7 @@ Visualizations
 # plt.legend(title='County', bbox_to_anchor=(1.05, 1), loc='upper left')
 # plt.grid()
 # plt.tight_layout()
+# plt.savefig("Figs/PoundsByCounty.svg")
 # plt.show()
 
 # # [x] Annual Catch Over Time
@@ -108,6 +110,7 @@ Visualizations
 # plt.legend()
 # plt.grid()
 # plt.tight_layout()
+# plt.savefig("Figs/StateTotalsHistorical.svg")
 # plt.show()
 
 # # [x] Value per Pound per county (Value/Pounds)
@@ -128,6 +131,7 @@ Visualizations
 # plt.legend(title='County', bbox_to_anchor=(1.05, 1), loc='upper left')
 # plt.grid()
 # plt.tight_layout()
+# plt.savefig("Figs/CountyRevenue.svg")
 # plt.show()
 
 # total_data = data[data['COUNTY'] == 'TOTAL']
@@ -147,45 +151,5 @@ Visualizations
 # plt.legend()
 # plt.grid()
 # plt.tight_layout()
+# plt.savefig("Figs/StateRevenue.svg")
 # plt.show()
-
-import plotly.express as px
-
-# Filter the data for the most recent year
-latest_year = filtered_data['YEAR'].max()
-latest_data = filtered_data[filtered_data['YEAR'] == latest_year]
-
-import requests
-import json
-
-# Load the GeoJSON file
-response = requests.get('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json')
-geojson_data = response.json()
-
-# Print the first few features to inspect their properties
-print(json.dumps(geojson_data['features'][:5], indent=2))
-
-# # Create the choropleth map
-# fig = px.choropleth(
-#     latest_data,
-#     geojson='https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json',
-#     locations='FIPS',  # Use the FIPS codes
-#     featureidkey="properties.fips",  # Match GeoJSON's FIPS field
-#     color='VALUE',
-#     hover_name='COUNTY',
-#     color_continuous_scale='Viridis',
-#     title=f"Lobster Value by County for {latest_year}"
-# )
-
-# # Update the layout for better visualization
-# fig.update_geos(
-#     visible=False,
-#     projection_type="albers usa"
-# )
-# fig.update_layout(
-#     margin={"r":0,"t":0,"l":0,"b":0},
-#     coloraxis_colorbar=dict(title="Value (in $)")
-# )
-
-# # Show the map
-# fig.show()
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# LobsterHauls
-A personal project exploring historical and current annual lobster hauls in the Gulf of Maine. 
+# Lobster Hauls
+I may be from Ohio but I have a close relationship with Maine's lobster fishery. Every summer since 1989,
+my family and I have gathered on Cabbage Island where we host daily tradtional Downeast clambakes for 180 guests.
+Maine has one of the healthiest fisheries in the region due to it's tough and longstanding regulations. Maine was the first state to enforce and standard minimum and maximum on lobster catches. Meaning, if a lobster is too small or too large, it must be returned to the ocean. This has been the way for over 100 years. 
+
+As the Gulf of Maine continues to warm, there is concern that lobsters will become less prevalent in Maine waters as they migrate further north. Out of curiosity, I scoured the internet for annual landing data and found [this] from Maine's Department of Marine Resources. The record tracks lobster hauls by county going back to 1950 and includes the weight and value of the lobster haul.
+
+# About the Data
+County level data for this project was retrieved from the Department of Marine Resources for the State of Maine and is the report of commercial lobster landings by county and by year. The database has 511 entries from 1964 to 2023 and covers the pounds caught and the value of that year's haul.
+
+Counties covered by this report are seven coastal Maine counties: York, Cumberland, Sadahoc, Lincoln, Knox, Washington, Hancock.
+
+Weights in this report are recorded in pounds, the y-axis on the visualizations is scaled to allow for this. 
+Value is recorded in dollars. 
+
+[source: https://www.maine.gov/dmr/sites/maine.gov.dmr/files/inline-files/lobster.county_0.pdf]
+
+Higher level data was retrieved from NOAA going back to 1950 but at this time, the data is unused in this project. Future research with this dataset will include analyzing fishery patterns over time beyond just lobster. 
+[source: https://www.maine.gov/dmr/sites/maine.gov.dmr/files/inline-files/lobster.county_0.pdf]
+
+# Results
+
+Value of Annual Lobster Haul by County
+![Annual Value](Figs\CountyRevenue.svg "Title")
